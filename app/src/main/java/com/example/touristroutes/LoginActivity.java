54750a778dbf114b202ae2d354b9c26cf6b1512c
Bryan <bgto.dk@gmail.com>
2021-11-06 11:12:49 -0500

    primer commit

---
+++ app/src/main/java/com/example/touristroutes/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.touristroutes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button loginBtn,regBtn;
+    private String user;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password=findViewById(R.id.password);
+        loginBtn=findViewById(R.id.loginBtn);
+        regBtn=findViewById(R.id.registerBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user=email.getText().toString();
+                String psw=password.getText().toString();
+                if (user.equals("btoribio") && psw.equals("123")){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
